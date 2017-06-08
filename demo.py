b0eefafc2c6b586eecac69f4d72e084be28ff63a
James <jdgelok@gmail.com>
2017-06-08 10:19:11 -0400

    Polish

--- demo.py
+++ demo.py
@@ -6,14 +6,13 @@ from sklearn import neural_network
 #   ║║├─┤ │ ├─┤
 #  ═╩╝┴ ┴ ┴ ┴ ┴
 
-#    [height, weight, shoe size]
+#   [meter,kg,shoe size]
 X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], 
-	 [166, 65, 40], [190, 90, 47], [175, 64, 39], [177, 70, 40], 
-	 [159, 55, 37], 
-	 [171, 75, 42], [181, 85, 43]]
+     [166, 65, 40], [190, 90, 47], [175, 64, 39], [177, 70, 40], [159, 55, 37], 
+     [171, 75, 42], [181, 85, 43]]
 
 Y = ['male', 'male', 'female', 'female', 
-	 'male', 'male', 'female', 'female', 'female', 
+     'male', 'male', 'female', 'female', 'female', 
      'male', 'male']
 
 #  ╔╦╗┌─┐┌─┐┬┌─┐┬┌─┐┌┐┌  ╔╦╗┬─┐┌─┐┌─┐
@@ -27,8 +26,8 @@ Y = ['male', 'male', 'female', 'female',
 #  ╩ ╩└─┘┴─┘┴ ┴   ╩═╝┴ ┴ ┴ └─┘┴└─  ╩  └─┘┴└─└─┘└─┘┴   ┴ ┴└─└─┘┘└┘
 
 #clf is short for classifier
-clfDT   = tree.DecisionTreeClassifier()
-clfGNB  = naive_bayes.GaussianNB()
+clfDT  = tree.DecisionTreeClassifier()
+clfGNB = naive_bayes.GaussianNB()
 clfMLP = neural_network.MLPClassifier()
 
 # The .fit method trains the decision tree on our dataset
@@ -41,7 +40,7 @@ prediction = clfDT.predict([[190,70,43]])
 prediction = clfGNB.predict([[190,70,43]])
 prediction = clfMLP.predict([[190,70,43]])
 
-print("Using a Decision Tree classifier, predition: " + str(prediction))
-print("Using a Gaussian Naive Bayes classifier, predition: " + str(prediction))
+print("Using a Decision Tree classifier, predition: "          + str(prediction))
+print("Using a Gaussian Naive Bayes classifier, predition: "   + str(prediction))
 print("Using a Multi-layer Perceptron classifier, predition: " + str(prediction))
 
